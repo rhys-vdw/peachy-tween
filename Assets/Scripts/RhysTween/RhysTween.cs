0449d5f6e4f0d53251c3454550f5447fab926259
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-12 23:07:35 +1000

    Move logic into RhysTween

--- Assets/Scripts/RhysTween/RhysTween.cs
+++ Assets/Scripts/RhysTween/RhysTween.cs
@@ -1,10 +1,23 @@
 using System;
 using UnityEngine;
+using Leopotam.EcsLite;
+using System.Collections.Generic;
 
 namespace RhysTween {
-  public static class RhysTween {
-    static TweenRunner Runner => TweenRunner.Instance;
+  internal class RunState {
+    public float DeltaTime;
+    public EcsFilter GroupFilter;
+  }
 
+  public struct Tween {
+    readonly internal EcsPackedEntity _entity;
+
+    public Tween(EcsPackedEntity entity) {
+      _entity = entity;
+    }
+  }
+
+  public static class RhysTween {
 #region Tween factories
 
     public static Tween TRotation(this Transform transform, Quaternion endValue, float duration) =>
@@ -25,42 +38,50 @@ namespace RhysTween {
     public static Tween TLocalPosition(this Transform transform, Vector3 endValue, float duration) =>
       Tween(transform.localPosition, v => transform.localPosition = v, endValue, duration);
 
-    public static Tween Tween<T>(T from, Action<T> onChange, T to, float duration) {
-      var runner = Runner;
-      var tween = runner.CreateTween(from, onChange, to, duration);
-      return tween;
-    }
+    public static Tween Tween<T>(T from, Action<T> onChange, T to, float duration) =>
+      CreateTween(from, onChange, to, duration);
 
 #endregion
 #region Pause
 
-    public static bool IsPaused(this Tween tween) => Runner.IsPaused(tween);
+    public static bool IsPaused(this Tween tween) =>
+      Entity(tween, out var entity) && IsPaused(entity);
 
-    public static Tween Pause(this Tween tween) {
-      Runner.Pause(tween);
-      return tween;
+    public static void Pause(this Tween tween) {
+      if (
+        Entity(tween, out var entity) &&
+        !IsPaused(entity)
+      ) {
+        _world.AddComponent<Paused>(entity);
+      }
     }
 
-    public static Tween Resume(this Tween tween) {
-      Runner.Resume(tween);
-      return tween;
+    public static void Resume(this Tween tween) {
+      if (Entity(tween, out var entity)) {
+        _world.DelComponent<Paused>(entity);
+      }
     }
+
+    static bool IsPaused(int entity) =>
+      _world.HasComponent<Paused>(entity);
 
 #endregion
-#region Update
+#region Group
 
-    public static Tween SetUpdate(this Tween tween) {
-      Runner.SetGroup<Update>(tween);
-      return tween;
-    }
+    public static Tween SetUpdate(this Tween tween) => tween.SetGroup<Update>();
 
-    public static Tween SetFixedUpdate(this Tween tween) {
-      Runner.SetGroup<FixedUpdate>(tween);
-      return tween;
-    }
+    public static Tween SetFixedUpdate(this Tween tween) => tween.SetGroup<FixedUpdate>();
 
-    public static Tween SetManualUpdate(this Tween tween) {
-      Runner.SetGroup<ManualUpdate>(tween);
+    public static Tween SetManualUpdate(this Tween tween) => tween.SetGroup<ManualUpdate>();
+
+    public static Tween SetGroup<TUpdate>(this Tween tween) where TUpdate : struct {
+      if (Entity(tween, out var entity)) {
+        // TODO: Make dynamic.
+        _world.DelComponent<Update>(entity);
+        _world.DelComponent<FixedUpdate>(entity);
+        _world.DelComponent<ManualUpdate>(entity);
+        _world.AddComponent<TUpdate>(entity);
+      }
       return tween;
     }
 
@@ -71,14 +92,28 @@ namespace RhysTween {
       if (count < 0) {
         throw new ArgumentOutOfRangeException(nameof(count), count, "Must not be negative");
       }
-      if (count > 0) {
-        Runner.SetLooping(tween, count);
+      return tween.SetLooping(count);
+    }
+
+    public static Tween StopLoop(this Tween tween) {
+      if (Entity(tween, out var entity)) {
+        _world.DelComponent<Loop>(entity);
       }
       return tween;
     }
 
-    public static Tween LoopForever(this Tween tween) {
-      Runner.SetLooping(tween, -1);
+    public static Tween LoopForever(this Tween tween) =>
+      tween.SetLooping(-1);
+
+    static Tween SetLooping(this Tween tween, int remaining = -1) {
+      if (Entity(tween, out var entity)) {
+        if (remaining == 0) {
+          _world.DelComponent<Loop>(entity);
+        } else {
+          ref var loop = ref _world.EnsureComponent<Loop>(entity);
+          loop.Remaining = remaining;
+        }
+      }
       return tween;
     }
 
@@ -86,8 +121,92 @@ namespace RhysTween {
 #region Callbacks
 
     public static Tween OnComplete(this Tween tween, Action onComplete) {
-      Runner.SetOnComplete(tween, onComplete);
+      if (Entity(tween, out var entity)) {
+        ref var c = ref _world.EnsureComponent<OnComplete>(entity);
+        c.Callback = onComplete;
+      }
       return tween;
+    }
+
+#endregion
+#region Ecs
+
+    static EcsWorld _world;
+    static EcsSystems _systems;
+    static readonly RunState _runState = new ();
+    static UnityLifecycle _lifecycle;
+
+#pragma warning disable IDE0051
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize() {
+      _world = new ();
+      _systems = new EcsSystems(_world, _runState)
+        .Add(new ActivateGroupSystem())
+        .Add(new ProgressSystem())
+        .Add(CreateChangeSystem<float>(Mathf.LerpUnclamped))
+        .Add(CreateChangeSystem<Vector2>(Vector2.LerpUnclamped))
+        .Add(CreateChangeSystem<Vector3>(Vector3.LerpUnclamped))
+        .Add(CreateChangeSystem<Quaternion>(Quaternion.LerpUnclamped))
+        .Add(new DeactivateGroupSystem())
+        .Add(new LoopSystem())
+        .Add(new CompleteSystem());
+      _systems.Init();
+
+      var go = new GameObject("RhysTween_UnityLifecycle");
+      _lifecycle = go.AddComponent<UnityLifecycle>();
+      UnityEngine.Object.DontDestroyOnLoad(_lifecycle);
+    }
+#pragma warning restore IDE0051
+
+    internal static void Destroy() => _world.Destroy();
+
+#endregion
+#region Tweens
+
+  static Tween CreateTween<T>(T from, Action<T> onChange, T to, float duration) {
+    var entity = _world.NewEntity();
+    _world.AddComponent(entity, new TweenConfig<T>(from, to, onChange));
+    _world.AddComponent(entity, new TweenState(duration));
+    _world.AddComponent<Update>(entity);
+    return new Tween(_world.PackEntity(entity));
+  }
+
+#endregion
+#region Run
+
+    static readonly Dictionary<Type, EcsFilter> _groupFilters = new();
+
+    static EcsFilter GroupFilter<TGroup>() where TGroup : struct {
+      if (!_groupFilters.TryGetValue(typeof(TGroup), out var filter)) {
+        filter = _world.Filter<TGroup>().End();
+        _groupFilters.Add(typeof(TGroup), filter);
+      }
+      return filter;
+    }
+
+    public static void Run<TGroup>(float deltaTime) where TGroup : struct{
+      _runState.DeltaTime = deltaTime;
+      _runState.GroupFilter = GroupFilter<TGroup>();
+      _systems.Run();
+    }
+
+#endregion
+#region Private
+
+    static ChangeSystem<TValue> CreateChangeSystem<TValue>(Lerp<TValue> lerp) {
+      var filter = _world.Filter<TweenConfig<TValue>>().Inc<Active>().Exc<Paused>().End();
+      return CreateChangeSystem(filter, lerp);
+    }
+
+    static ChangeSystem<T> CreateChangeSystem<T>(EcsFilter filter, Lerp<T> lerp) =>
+      new (filter, lerp);
+
+    static bool Entity(Tween tween, out int entity) {
+      if (!tween._entity.Unpack(_world, out entity)) {
+        Debug.LogWarning($"Tween is no longer active");
+        return false;
+      }
+      return true;
     }
 
 #endregion
--- Assets/Scripts/RhysTween/TweenRunner.cs
+++ Assets/Scripts/RhysTween/TweenRunner.cs
@@ -1,164 +1,17 @@
 using UnityEngine;
-using Leopotam.EcsLite;
-using System;
-using System.Collections.Generic;
 
 namespace RhysTween {
-  internal class RunState {
-    public float DeltaTime;
-    public EcsFilter GroupFilter;
-  }
-
-  public struct Tween {
-    readonly internal EcsPackedEntity _entity;
-
-    public Tween(EcsPackedEntity entity) {
-      _entity = entity;
-    }
-  }
-
-  internal class TweenRunner : MonoBehaviour {
-    EcsWorld _world;
-    EcsSystems _systems;
-    readonly RunState _runState = new ();
-
-#region Singleton
-
-    static TweenRunner _instance;
-
-    public static TweenRunner Instance {
-      get {
-        if (_instance == null) {
-          var go = new GameObject("TweenRunner");
-          _instance = go.AddComponent<TweenRunner>();
-          DontDestroyOnLoad(_instance);
-        }
-        return _instance;
-      }
-    }
-
-#endregion
-#region Tweens
-
-  public Tween CreateTween<T>(T from, Action<T> onChange, T to, float duration) {
-    var entity = _world.NewEntity();
-    _world.AddComponent(entity, new TweenConfig<T>(from, to, onChange));
-    _world.AddComponent(entity, new TweenState(duration));
-    _world.AddComponent<Update>(entity);
-    return new Tween(_world.PackEntity(entity));
-  }
-
-  public void SetOnComplete(Tween tween, Action onComplete) {
-    if (Entity(tween, out var entity)) {
-      ref var c = ref _world.EnsureComponent<OnComplete>(entity);
-      c.Callback = onComplete;
-    }
-  }
-
-  public void SetLooping(Tween tween, int remaining = -1) {
-    if (Entity(tween, out var entity)) {
-      ref var loop = ref _world.EnsureComponent<Loop>(entity);
-      loop.Remaining = remaining;
-    }
-  }
-
-  public void SetGroup<TUpdate>(Tween tween) where TUpdate : struct {
-    if (Entity(tween, out var entity)) {
-      _world.DelComponent<Update>(entity);
-      _world.DelComponent<FixedUpdate>(entity);
-      _world.DelComponent<ManualUpdate>(entity);
-      _world.AddComponent<TUpdate>(entity);
-    }
-  }
-
-  public bool IsPaused(Tween tween) =>
-    Entity(tween, out var entity) && IsPaused(entity);
-
-  public void Pause(Tween tween) {
-    if (
-      Entity(tween, out var entity) &&
-      !IsPaused(entity)
-    ) {
-      _world.AddComponent<Paused>(entity);
-    }
-  }
-
-  public void Resume(Tween tween) {
-    if (Entity(tween, out var entity)) {
-      _world.DelComponent<Paused>(entity);
-    }
-  }
-
-#endregion
-#region Run
-
-    readonly Dictionary<Type, EcsFilter> _groupFilters = new();
-
-    EcsFilter GroupFilter<TGroup>() where TGroup : struct {
-      if (!_groupFilters.TryGetValue(typeof(TGroup), out var filter)) {
-        filter = _world.Filter<TGroup>().End();
-        _groupFilters.Add(typeof(TGroup), filter);
-      }
-      return filter;
-    }
-
-    public void ManualUpdate(float deltaTime) => Run<ManualUpdate>(deltaTime);
-
-    public void Run<TGroup>(float deltaTime) where TGroup : struct{
-      _runState.DeltaTime = deltaTime;
-      _runState.GroupFilter = GroupFilter<TGroup>();
-      _systems.Run();
-    }
-
-#endregion
-#region Private
-
-    bool IsPaused(int entity) => _world.HasComponent<Paused>(entity);
-
-    ChangeSystem<TValue> CreateChangeSystem<TValue>(Lerp<TValue> lerp) {
-      var filter = _world.Filter<TweenConfig<TValue>>().Inc<Active>().Exc<Paused>().End();
-      return CreateChangeSystem(filter, lerp);
-    }
-
-    static ChangeSystem<T> CreateChangeSystem<T>(EcsFilter filter, Lerp<T> lerp) =>
-      new (filter, lerp);
-
-    bool Entity(Tween tween, out int entity) {
-      if (!tween._entity.Unpack(_world, out entity)) {
-        Debug.LogWarning($"Tween is no longer active");
-        return false;
-      }
-      return true;
-    }
-
-#endregion
-#region Unity lifecycle
+  internal class UnityLifecycle : MonoBehaviour {
 #pragma warning disable IDE0051
 
-    void Awake() {
-      _world = new ();
-      _systems = new EcsSystems(_world, _runState)
-        .Add(new ActivateGroupSystem())
-        .Add(new ProgressSystem())
-        .Add(CreateChangeSystem<float>(Mathf.LerpUnclamped))
-        .Add(CreateChangeSystem<Vector2>(Vector2.LerpUnclamped))
-        .Add(CreateChangeSystem<Vector3>(Vector3.LerpUnclamped))
-        .Add(CreateChangeSystem<Quaternion>(Quaternion.LerpUnclamped))
-        .Add(new DeactivateGroupSystem())
-        .Add(new LoopSystem())
-        .Add(new CompleteSystem());
-      _systems.Init();
-    }
+    void Update() => RhysTween.Run<Update>(Time.deltaTime);
 
-    void Update() => Run<Update>(Time.deltaTime);
+    void LateUpdate() => RhysTween.Run<LateUpdate>(Time.deltaTime);
 
-    void LateUpdate() => Run<LateUpdate>(Time.deltaTime);
+    void FixedUpdate() => RhysTween.Run<FixedUpdate>(Time.deltaTime);
 
-    void FixedUpdate() => Run<FixedUpdate>(Time.deltaTime);
-
-    void OnDestroy() => _world.Destroy();
+    void OnDestroy() => RhysTween.Destroy();
 
 #pragma warning restore IDE0051
-#endregion
   }
 }
