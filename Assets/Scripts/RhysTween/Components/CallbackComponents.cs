1dd9f5e97f2726d69f3ee3e11b777bd4edbd5436
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-13 23:55:36 +1000

    Move CallbackComponents into their own file

---
+++ Assets/Scripts/RhysTween/Components/CallbackComponents.cs
@@ -0,0 +1,30 @@
+using System;
+using Leopotam.EcsLite;
+
+namespace RhysTween {
+  internal interface ICallback {
+    Action Callback { get; set; }
+  }
+
+  internal struct OnComplete : ICallback {
+    public Action Callback { get; set; }
+  }
+
+  internal struct OnKill : ICallback {
+    public Action Callback { get; set; }
+  }
+
+  static class CallbackUtility {
+    internal static void Invoke<T>(this EcsWorld world, int entity) where T : struct, ICallback {
+      var callbackPool = world.GetPool<T>();
+      if (callbackPool.Has(entity)) {
+        callbackPool.Get(entity).Callback();
+      }
+    }
+
+    internal static void AddHandler<T>(this EcsWorld world, int entity, Action callback) where T : struct, ICallback {
+      ref var cb = ref world.EnsureComponent<T>(entity);
+      cb.Callback += callback;
+    }
+  }
+}
