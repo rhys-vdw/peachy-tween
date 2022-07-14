751a6a36987820e5b1950ab58e812903fc411203
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-15 02:59:29 +1000

    Add per-dimension transform position methods

--- Assets/Scripts/PeachyTween/Extensions/TransformExtensions.cs
+++ Assets/Scripts/PeachyTween/Extensions/TransformExtensions.cs
@@ -17,6 +17,31 @@ namespace PeachyTween {
     public static Tween TweenPosition(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.position, endValue, v => transform.position = v, duration);
 
+    public static Tween TweenPositionX(this Transform transform, float endValue, float duration) =>
+      Peachy.Tween(
+        transform.position.x,
+        endValue,
+        v => transform.position = transform.position.WithX(v),
+        duration
+      );
+
+    public static Tween TweenPositionY(this Transform transform, float endValue, float duration) =>
+      Peachy.Tween(
+        transform.position.y,
+        endValue,
+        v => transform.position = transform.position.WithY(v),
+        duration
+      );
+
+    public static Tween TweenPositionZ(this Transform transform, float endValue, float duration) =>
+      Peachy.Tween(
+        transform.position.z,
+        endValue,
+        v => transform.position = transform.position.WithZ(v),
+        duration
+      );
+
+
     public static Tween TweenLocalPosition(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.localPosition, endValue, v => transform.localPosition = v, duration);
   }
