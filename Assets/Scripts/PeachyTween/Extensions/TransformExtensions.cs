6543dabc8cc23426ef8d581f57f0dbb1cf9ce345
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-15 03:03:23 +1000

    Add regions to transform extensions

--- Assets/Scripts/PeachyTween/Extensions/TransformExtensions.cs
+++ Assets/Scripts/PeachyTween/Extensions/TransformExtensions.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 namespace PeachyTween {
   public static class TransformExtensions {
+#region Rotation
+
     public static Tween TweenRotation(this Transform transform, Quaternion endValue, float duration) =>
       Peachy.Tween(transform.rotation, endValue, v => transform.rotation = v, duration);
 
     public static Tween TweenRotation(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.rotation, Quaternion.Euler(endValue), v => transform.rotation = v, duration);
 
+#endregion
+#region Local rotation
+
     public static Tween TweenLocalRotation(this Transform transform, Quaternion endValue, float duration) =>
       Peachy.Tween(transform.localRotation, endValue, v => transform.localRotation = v, duration);
 
     public static Tween TweenLocalRotation(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.localRotation, Quaternion.Euler(endValue), v => transform.localRotation = v, duration);
+
+#endregion
+#region Position
 
     public static Tween TweenPosition(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.position, endValue, v => transform.position = v, duration);
@@ -41,8 +49,12 @@ namespace PeachyTween {
         duration
       );
 
+#endregion
+#region Local position
 
     public static Tween TweenLocalPosition(this Transform transform, Vector3 endValue, float duration) =>
       Peachy.Tween(transform.localPosition, endValue, v => transform.localPosition = v, duration);
+
+#endregion
   }
 }
