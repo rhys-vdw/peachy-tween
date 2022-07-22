ef5ded3784f16ab9f742371ccaf51a4eb8bed39b
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-22 23:44:12 +1000

    Document Tween functions

--- Runtime/Peachy.cs
+++ Runtime/Peachy.cs
@@ -6,33 +6,68 @@ namespace PeachyTween {
 #region Tween factory
 
     /// <summary>
-    /// Create a new tween.
+    /// Create a new <c>float</c> tween.
     /// </summary>
     /// <param name="from">The starting value.</param>
     /// <param name="to">The end value.</param>
     /// <param name="duration">Total tween duration in seconds.</param>
     /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
-    /// <returns>The newly created tween instance.</returns>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(float from, float to, float duration, Action<float> onChange) =>
       CreateTween(from, to, duration, onChange);
 
-    /// <inheritdoc cref="Tween" />
+    /// <summary>
+    /// Create a new <c>Vector2</c> tween.
+    /// </summary>
+    /// <param name="from">The starting value.</param>
+    /// <param name="to">The end value.</param>
+    /// <param name="duration">Total tween duration in seconds.</param>
+    /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(Vector2 from, Vector2 to, float duration, Action<Vector2> onChange) =>
       CreateTween(from, to, duration, onChange);
 
-    /// <inheritdoc cref="Tween" />
+    /// <summary>
+    /// Create a new <c>Vector3</c> tween.
+    /// </summary>
+    /// <param name="from">The starting value.</param>
+    /// <param name="to">The end value.</param>
+    /// <param name="duration">Total tween duration in seconds.</param>
+    /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(Vector3 from, Vector3 to, float duration, Action<Vector3> onChange) =>
       CreateTween(from, to, duration, onChange);
 
-    /// <inheritdoc cref="Tween"/>
+    /// <summary>
+    /// Create a new <c>Vector4</c> tween.
+    /// </summary>
+    /// <param name="from">The starting value.</param>
+    /// <param name="to">The end value.</param>
+    /// <param name="duration">Total tween duration in seconds.</param>
+    /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(Vector4 from, Vector4 to, float duration, Action<Vector4> onChange) =>
       CreateTween(from, to, duration, onChange);
 
-    /// <inheritdoc cref="Tween"/>
+    /// <summary>
+    /// Create a new <c>Quaternion</c> tween.
+    /// </summary>
+    /// <param name="from">The starting value.</param>
+    /// <param name="to">The end value.</param>
+    /// <param name="duration">Total tween duration in seconds.</param>
+    /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(Quaternion from, Quaternion to, float duration, Action<Quaternion> onChange) =>
       CreateTween(from, to, duration, onChange);
 
-    /// <inheritdoc cref="Tween"/>
+    /// <summary>
+    /// Create a new <c>Color</c> tween.
+    /// </summary>
+    /// <param name="from">The starting value.</param>
+    /// <param name="to">The end value.</param>
+    /// <param name="duration">Total tween duration in seconds.</param>
+    /// <param name="onChange">A callback that will be invoked every time the tween value changes.</param>
+    /// <returns>The newly created tween.</returns>
     public static Tween Tween(Color from, Color to, float duration, Action<Color> onChange) =>
       CreateTween(from, to, duration, onChange);
 
