7019dd7fb7fdbcba43397d8511623d9ac4e5cf42
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-26 18:41:44 +1000

    Add Sequence.ToTween and cast

--- Runtime/Sequence.cs
+++ Runtime/Sequence.cs
@@ -13,7 +13,15 @@ namespace PeachyTween {
       _entity = entity;
     }
 
-#region Sequence
+#region Tween
+
+    public static explicit operator Tween(Sequence sequence) =>
+      sequence.ToTween();
+
+    public Tween ToTween() => new Tween(_entity);
+
+#endregion
+#region Sequence operations
 
     public Sequence Append(Tween tween) {
       if (
@@ -41,165 +49,6 @@ namespace PeachyTween {
         tween.Entity(out var tweenEntity)
       ) {
         Core.Insert(sequenceEntity, tweenEntity, time);
-      }
-      return this;
-    }
-
-#endregion
-#region Control
-
-    /// <summary>
-    /// Restart the tween and unpause it.<para/>
-    /// Equivalent to <c>tween.Rewind().Resume()</c><para/>
-    /// </summary>
-    /// <seealso cref="Rewind"/>
-    /// <seealso cref="Resume"/>
-    // public Sequence Restart() => Rewind().Resume();
-
-    /// <summary>
-    /// Return the tween to the start.<para/>
-    /// Equivalent to <c cref="GoTo">GoTo(0)</c>.<para/>
-    /// </summary>
-    /// <seealso cref="GoTo"/>
-    /// <returns>This tween.</returns>
-    public Sequence Rewind() => GoTo(0);
-
-    /// <summary>
-    /// Set the interal time of a tween.<para/>
-    /// </summary>
-    /// <param name="elapsed">
-    /// The time to set the tween to. <c>0</c> will rewind the tween to the
-    /// start, and passing the tween's duration will fast-forward to the end.
-    /// </param>
-    /// <returns>This tween.</returns>
-    public Sequence GoTo(float elapsed) {
-      if (Entity(out var entity)) {
-        Core.GoTo(entity, elapsed);
-      }
-      return this;
-    }
-
-    /// <summary>
-    /// Complete the tween, including all loops.<para/>
-    /// </summary>
-    /// <remarks>
-    /// When the tween is updated it will call its <c
-    /// cref="OnComplete">OnComplete</c> callback, and will be killed if not
-    /// <see cref="Preserve">preserved</see>.<para/>
-    ///
-    /// To complete the tween immediately, call <c cref="Sync">tween.Sync()</c>.
-    /// </remarks>
-    /// <returns>This tween.</returns>
-    public Sequence Complete() {
-      if (Entity(out var entity)) {
-        Core.Complete(entity);
-      }
-      return this;
-    }
-
-    /// <summary>
-    /// Check if the tween has ended.<para/>
-    /// </summary>
-    /// <remarks>
-    /// This is useful for tweens that are <see cref="Preserve">preserved</see>,
-    /// as any other tween will be killed upon completion.
-    /// </remarks>
-    /// <returns>`true` if tween is complete; `false` if tween is not complete.</returns>
-    public bool IsComplete() =>
-      Entity(out var entity) &&
-      Core.IsComplete(entity);
-
-    /// <summary>
-    /// Reverse the direction of this tween.
-    /// </summary>
-    /// <returns>This tween.</returns>
-    public Sequence Reverse() {
-      if (Entity(out var entity)) {
-        Core.Reverse(entity);
-      }
-      return this;
-    }
-
-    /// <summary>
-    /// Run this tween from end to start.
-    /// </summary>
-    /// <returns>This tween.</returns>
-    public Sequence From() {
-      if (Entity(out var entity)) {
-        Core.From(entity);
-      }
-      return this;
-    }
-
-#endregion
-#region Easing
-
-    /// <summary>
-    /// Clears the easing function, defaulting to linear interpolation.
-    /// </summary>
-    /// <seealso cref="Ease"/>
-    /// <param name="easeFunc">An easing function.</param>
-    /// <returns>This tween.</returns>
-    public Sequence ClearEase() {
-      if (Entity(out var entity)) {
-        Core.ClearEase(entity);
-      }
-      return this;
-    }
-
-    /// <summary>
-    /// Set the easing function for this tween.
-    /// </summary>
-    /// <param name="ease">A standard easing function.</param>
-    /// <returns>This tween.</returns>
-    public Sequence Ease(Ease ease) =>
-      ease == PeachyTween.Ease.Linear
-        ? ClearEase()
-        : Ease(ease.ToFunc());
-
-    /// <inheritdoc cref="Ease" />
-    /// <param name="easeFunc">An easing function.</param>
-    public Sequence Ease(EaseFunc easeFunc) {
-      if (Entity(out var entity)) {
-        Core.Ease(entity, easeFunc);
-      }
-      return this;
-    }
-
-    /// <inheritdoc cref="Ease" />
-    /// <param name="animationCurve">An animation curve from (0,0) to (1,1).</param>
-    public Sequence Ease(AnimationCurve animationCurve) {
-      _ = animationCurve ?? throw new ArgumentNullException(nameof(animationCurve));
-      return Ease(animationCurve.Evaluate);
-    }
-
-#endregion
-#region Preserve
-
-    /// <summary>
-    /// Prevent tween from being automatically killed when it completes.
-    /// </summary>
-    /// <remarks>
-    /// Use this to create a replayable tween.
-    /// </remarks>
-    /// <seealso cref="ClearPreserve"/>
-    /// <returns>This tween.</returns>
-    public Sequence Preserve() {
-      if (Entity(out var entity)) {
-        Core.Preserve(entity);
-      }
-      return this;
-    }
-
-    /// <summary>
-    /// Cancels <see cref="Preserve"><c>Preserve</c></see>, allowing the tween
-    /// to be killed when it completes.
-    /// </summary>
-    /// <seealso cref="Preserve"/>
-    /// <returns>This tween.</returns>
-    public Sequence ClearPreserve() {
-      if (Entity(out var entity)) {
-        Core.ClearPreserve(entity);
       }
       return this;
     }
