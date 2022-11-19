2d3dd08f9d824606348f422e9dbca5a8568fb5da
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-11-19 18:14:05 +1100

    Calculate sequence member state enum

--- Runtime/Systems/SequenceSystem.cs
+++ Runtime/Systems/SequenceSystem.cs
@@ -11,9 +11,16 @@ namespace PeachyTween {
       _filter = _world.Filter<SequenceMember>().End();
     }
 
+    enum State {
+      Before,
+      Active,
+      After
+    }
+
     public void Run(EcsSystems systems) {
       var statePool = _world.GetPool<TweenState>();
       var activePool = _world.GetPool<Active>();
+      var completePool = _world.GetPool<Complete>();
       foreach (var entity in _filter) {
         ref var member = ref _world.GetComponent<SequenceMember>(entity);
         if (activePool.Has(member.SequenceEntity)) {
@@ -21,32 +28,46 @@ namespace PeachyTween {
           ref var sequenceState = ref statePool.Get(member.SequenceEntity);
           ref var sequenceActive = ref activePool.Get(member.SequenceEntity);
 
+          // Calculate previous state.
+          var prevState = tweenState.Elapsed switch {
+            var e when e < 0 => State.Before,
+            var e when e > tweenState.Duration => State.After,
+            _ => State.Active,
+          };
+
           // Update tween elapsed time.
           var easedElapse = sequenceActive.Progress * sequenceState.Duration;
           tweenState.Elapsed = easedElapse - member.StartTime;
 
-          // Work out if the tween is complete.
-          var isReversed = _world.HasComponent<Reverse>(member.SequenceEntity);
-          var isComplete = isReversed
-            ? tweenState.Elapsed <= 0
-            : tweenState.Elapsed >= tweenState.Duration;
+          // Calculate next state.
+          var nextState = tweenState.Elapsed switch {
+            var e when e < 0 => State.Before,
+            var e when e > tweenState.Duration => State.After,
+            _ => State.Active,
+          };
 
-          if (isComplete) {
-            if (!_world.HasComponent<Complete>(entity)) {
-              // If we've exceeded duration, but the tween is yet to be marked
-              // complete, activate it so it can be completed.
-              activePool.Add(entity);
-
-              // Mark the tween complete.
-              _world.AddComponent<Complete>(entity);
-            }
-          } else {
+          if (nextState == State.Active) {
             // Mark tween incomplete.
             _world.DelComponent<Complete>(entity);
 
-            // If the tween is active, mark it active so it will progress.
-            if (tweenState.Elapsed > 0 && tweenState.Elapsed < tweenState.Duration) {
+            // Mark tween active.
+            activePool.Add(entity);
+          } else {
+            // Ensure that tween is activated to do one last update.
+            if (nextState != prevState) {
               activePool.Add(entity);
+            }
+
+            // Work out if the tween is complete.
+            if (!completePool.Has(entity)) {
+              var isReversed = _world.HasComponent<Reverse>(member.SequenceEntity);
+              var isComplete = isReversed
+                ? nextState == State.Before
+                : nextState == State.After;
+
+              if (isComplete) {
+                completePool.Add(entity);
+              }
             }
           }
         }
